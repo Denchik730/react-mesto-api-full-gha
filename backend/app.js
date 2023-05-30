2545d591be98cf950fac8e83da5efc89a28505b3
Denis Gabbasov <gabbas.denn@mail.ru>
2023-05-30 18:24:35 +0300

    feat:убрать тест падения сервера для ревью

--- backend/app.js
+++ backend/app.js
@@ -36,12 +36,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(requestLogger);
 
-app.get('/crash-test', () => {
-  setTimeout(() => {
-    throw new Error('Сервер сейчас упадёт');
-  }, 0);
-});
-
 app.use(router);
 
 app.use(errorLogger);
